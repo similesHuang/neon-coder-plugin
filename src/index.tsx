59febd2cb1a45c924cdc67e90d88ee4e97e57fee
simileHuang <1959974025@qq.com>
2025-07-16 15:08:43 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+root.render(<App />);
